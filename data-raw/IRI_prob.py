ea1066ed175d0e3d60d6b8a2001d9c656229ba4f
Zack  Arno <zackarno@gmail.com>
2023-10-02 17:08:30 -0400

    download and initial explore of IRI data

---
+++ data-raw/IRI_prob.py
@@ -0,0 +1,32 @@
+
+# IRI only works on python versions less than 3.10 -- using 3.9.16 for this code
+
+
+# Download proability forecast
+from ochanticipy import create_country_config, CodAB, GeoBoundingBox, \
+                      IriForecastDominant, IriForecastProb
+
+country_config = create_country_config(iso3="som")
+
+codab = CodAB(country_config=country_config)
+codab.download()
+admin0 = codab.load(admin_level=0)
+
+geo_bounding_box = GeoBoundingBox.from_shape(admin0)
+
+
+iri_prob = IriForecastProb(country_config=country_config,
+                           geo_bounding_box=geo_bounding_box)
+iri_prob.download()
+iri_prob.process()
+# iri_prob_data = iri_prob.load()
+
+
+# Download dominant tercile data
+
+IriForecastDominant
+iri_dom = IriForecastDominant(country_config=country_config,
+                           geo_bounding_box=geo_bounding_box)
+iri_dom.download()
+iri_dom.process()
+# iri_prob_data = iri_prob.load()
