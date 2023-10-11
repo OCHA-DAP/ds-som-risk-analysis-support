3c7e56a323ab591a9a52053428dd6046eb6f1282
Zack  Arno <zackarno@gmail.com>
2023-10-11 16:08:09 -0400

    run zonal stats on juba and shabelle basins

--- data-raw/chirps_historical.R
+++ data-raw/chirps_historical.R
@@ -4,26 +4,47 @@ library(tidyrgee)
 library(targets)
 library(rhdx)
 library(janitor)
+library(sf)
 ee_Initialize()
 
-admin_level <-  c("adm0","adm1","adm2")[3]
+analysis_level <-  c("adm0","adm1","adm2","bas4")[4]
 
-som_adm0_levels <- c(
-  "som_admbnda_adm0_ocha_20230308",
-  "som_admbnda_adm1_ocha_20230308",
-  "som_admbnda_adm2_ocha_20230308"
-)
+adm_analysis <- str_detect(analysis_level,"^adm")
+if(adm_analysis){
+  som_adm0_levels <- c(
+    "som_admbnda_adm0_ocha_20230308",
+    "som_admbnda_adm1_ocha_20230308",
+    "som_admbnda_adm2_ocha_20230308"
+  )
 
-som_adm <- som_adm0_levels %>%
-  map(
-    ~ search_datasets("Somalia - Subnational Administrative Boundaries") %>%
-      pluck(1) %>%
-      get_resource(2) %>%
-      read_resource(layer = .x) %>%
-      clean_names() %>%
-      select(matches("^adm\\d_"))
-  ) %>%
-  set_names(c("adm0", "adm1", "adm2"))
+  som_adm <- som_adm0_levels %>%
+    map(
+      ~ search_datasets("Somalia - Subnational Administrative Boundaries") %>%
+        pluck(1) %>%
+        get_resource(2) %>%
+        read_resource(layer = .x) %>%
+        clean_names() %>%
+        select(matches("^adm\\d_"))
+    ) %>%
+    set_names(c("adm0", "adm1", "adm2"))
+
+  gdf_zone <- som_adm[[analysis_level]]
+}
+if(!adm_analysis){
+  cat("reading bas4\n")
+  gdb_basin <- file.path(
+    Sys.getenv("AA_DATA_DIR"),
+    "public",
+    "raw",
+    "som",
+    "hydrobasin.gpkg"
+  )
+  gdf_zone <- read_sf(gdb_basin, layer = "hybas3_af_lev03_v1c_shabelle_juba") %>%
+    rename(
+      geometry= "geom"
+    )
+
+}
 
 out_dir <- file.path(
   Sys.getenv("AA_DATA_DIR"),
@@ -44,14 +65,14 @@ tic_monthly_rainfall <- tic %>%
     stat = "sum"
   )
 
-som_adm[[admin_level]]
 
-fc_adm <- sf_as_ee(x = som_adm[[admin_level]])
 
-cat("begin extraction of zonal stats for ", admin_level,"\n")
+fc_zone <- sf_as_ee(x = gdf_zone)
+
+cat("begin extraction of zonal stats for ", analysis_level,"\n")
 df_rainfall_adm <- ee_extract_tidy(
   x = tic_monthly_rainfall,
-  y = fc_adm,
+  y = fc_zone,
   scale = 5566,
   stat = "mean",
   via = "drive"
@@ -64,7 +85,7 @@ write_csv(
   file = file.path(
     out_dir,
     paste0(
-      prefix_date,"_chirps_monthly_historical_",admin_level,".csv"
+      prefix_date,"_chirps_monthly_historical_",analysis_level,".csv"
     )
   )
 )
